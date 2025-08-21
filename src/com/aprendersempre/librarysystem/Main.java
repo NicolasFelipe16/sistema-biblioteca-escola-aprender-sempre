a65e54fb6e6efb2e47cdbb772395d9a52572bd6e
Nícolas Felipe <nicolas.felipef16@gmail.com>
2025-08-21 20:57:20 -0300

    Criação da lista 'loansList', implementação do método 'finishLoan' e método de exibição da lista de empréstimos, criação de getters de usuário e livro em 'Loan' e criação de método de remoção de livro da lista de livros emprestados pelo usuário.

--- src/com/aprendersempre/librarysystem/Main.java
+++ src/com/aprendersempre/librarysystem/Main.java
@@ -8,47 +8,31 @@ import com.aprendersempre.librarysystem.internal.User;
 
 public class Main {
     public static void main(String[] args) {
-        Book book0 = new Book("B-00", "Wagner");
-        Book book1 = new Book("B-01", "Nícolas");
-        Book book2 = new Book("B-02", "Matheus");
-        Book book3 = new Book("B-03", "Priscila");
-        Book book4 = new Book("B-04", "Camila");
-        Book book5 = new Book("B-05", "Vinícius");
-        Book book6 = new Book("B-06", "Ronaldo");
-        Book book7 = new Book("B-07", "Julia");
-        Book book8 = new Book("B-08", "Carla");
-        Book book9 = new Book("B-09", "Marcos");
+        Book livro_01 = new Book("Livro 01", "Autor 01");
+        Book livro_02 = new Book("Livro 02", "Autor 02");
+        User vinicius = new Teacher("Vinícius Louzada");
+        User nicolas = new Teacher("Nícolas Felipe");
 
         LoanService loanService = new LoanService();
 
-        // Professor > Limite 5 empréstimos simultâneos
-        User vinicius = new Teacher("Vinícius Louzada");
+        System.out.println("\n==============================");
+        System.out.println("1. Antes do empréstimo:\n");
+        System.out.println("Lista de livros emprestados: " + vinicius.getBorrowedBooksList());
+        System.out.println("Disponibilidade do livro: " + livro_01.isAvailable());
+        loanService.showFormatedLoansList();
 
-        // Tentativa de 6 empréstimos simultâneos
-        loanService.createLoan(book0, vinicius);
-        // Tentativa de empréstimo de livro indisponível
-        loanService.createLoan(book0, vinicius);
-        loanService.createLoan(book1, vinicius);
-        loanService.createLoan(book2, vinicius);
-        loanService.createLoan(book3, vinicius);
-        loanService.createLoan(book4, vinicius);
-        loanService.createLoan(book5, vinicius);
+        System.out.println("\n==============================");
+        loanService.createLoan(livro_01, vinicius);
+        System.out.println("2. Empréstimo feito:\n");
+        System.out.println("Lista de livros emprestados: " + vinicius.getBorrowedBooksList());
+        System.out.println("Disponibilidade do livro: " + livro_01.isAvailable());
+        loanService.showFormatedLoansList();
 
-        // Livros emprestados (5)
-        vinicius.showFormatedBorrowedBooksList();
-
-        // Estudante > Limite 3 empréstimos simultâneos
-        User nicolas = new Student("Nícolas Felipe");
-
-        // Tentativa de 4 empréstimos simultâneos
-        loanService.createLoan(book6, nicolas);
-        // Tentativa de empréstimo de livro indisponível
-        loanService.createLoan(book6, nicolas);
-        loanService.createLoan(book7, nicolas);
-        loanService.createLoan(book8, nicolas);
-        loanService.createLoan(book9, nicolas);
-
-        // Livros emprestados (3)
-        nicolas.showFormatedBorrowedBooksList();
+        System.out.println("\n==============================");
+        System.out.println("3. Empréstimo finalizado\n");
+        loanService.finishLoan(livro_01, vinicius);
+        System.out.println("Lista de livros emprestados: " + vinicius.getBorrowedBooksList());
+        System.out.println("Disponibilidade do livro: " + livro_01.isAvailable());
+        loanService.showFormatedLoansList();
     }
 }
--- src/com/aprendersempre/librarysystem/internal/LoanService.java
+++ src/com/aprendersempre/librarysystem/internal/LoanService.java
@@ -1,8 +1,11 @@
 package com.aprendersempre.librarysystem.internal;
 
+import java.util.ArrayList;
 
 public class LoanService {
-    public Loan createLoan(Book book, User user) {
+    ArrayList<Loan> loansList = new ArrayList<>();
+
+    public void createLoan(Book book, User user) {
         if (!book.isAvailable()) {
             throw new IllegalStateException("Book is not available.");
         }
@@ -13,6 +16,35 @@ public class LoanService {
         book.markAsUnavailable();
         user.addBorrowedBook(book);
 
-        return new Loan(book, user);
+        Loan loan = new Loan(book, user);
+        loansList.add(loan);
+    }
+
+    public void finishLoan(Book book, User user) {
+        this.loansList.removeIf(loan -> loan.getBook() == book);
+
+        book.markAsAvailable();
+        user.removeBorrowedBook(book);
+    }
+
+    public ArrayList<Loan> getLoansList() {
+        return loansList;
+    }
+
+    public void showFormatedLoansList() {
+        System.out.printf("""
+                ==============================
+                LOANS LIST (%d)
+                ==============================
+                """, loansList.size());
+
+        for (Loan loan : loansList) {
+            String bookTitle = loan.getBook().getTitle();
+            String userFullName = loan.getUser().getFullName();
+
+            System.out.println(bookTitle + " | " + userFullName);
+        }
+
+        System.out.println("==============================");
     }
 }
